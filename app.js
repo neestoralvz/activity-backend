eab89b4aea1ea96c48899ae1496f4261b623c50a
neestoralvz <nestor.alvz@gmail.com>
2024-07-12 12:00:57 -0600

    Configurar conexión a MongoDB Atlas y agregar IP a la lista blanca

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log('MongoDB conectado');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1);
